d3c4a4f9162e6a77dc5e38a999b95d5a34d4e06d
Evgeny Rusak <kasured@gmail.com>
2014-12-25 16:17:28 +0300

    [data-structures][exercise 3_25/26 - implement size and find maximum for custom tree using pattern matching]

--- src/datastructures/package.scala
+++ src/datastructures/package.scala
@@ -281,4 +281,23 @@ package object datastructures {
 
   }
 
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  object Tree {
+    // Exercise 3_25 - implement size of the tree
+    def size[A](tree: Tree[A]): Int = tree match {
+      case Leaf(_) => 1
+      case Branch(l,r) => 1 + size(l) + size(r)
+    }
+
+    // Exercise 3_26 - implement fin max element of the tree
+    def maximum(tree: Tree[Int]): Int = tree match {
+      case Leaf(v) => v
+      case Branch(l, r) => maximum(l) max maximum(r)
+    }
+
+  }
+
 }
