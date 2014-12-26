6acb02a70ebab14b843c030d1712a93244e4f959
Evgeny Rusak <kasured@gmail.com>
2014-12-26 14:15:04 +0300

    [data-structures][exercise 3_28 - implement map function for ADT tree]

--- src/datastructures/package.scala
+++ src/datastructures/package.scala
@@ -304,6 +304,11 @@ package object datastructures {
       case Branch(l,r) => 1 + (depth(l) max depth(r))
     }
 
+    // Exercise 3_28 - implement map function for this ADT structure
+    def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+      case Leaf(v) => Leaf(f(v))
+      case Branch(l,r) => Branch(map(l)(f), map(r)(f))
+    }
 
   }
 
