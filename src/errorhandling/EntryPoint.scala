fca5a658f53ee9bdb9492518573e5f8e0e2441d2
Evgeny Rusak <kasured@gmail.com>
2014-12-29 16:59:15 +0300

    [error-handling][exercise 4_2 - implement variance using flatMap]

--- src/errorhandling/EntryPoint.scala
+++ src/errorhandling/EntryPoint.scala
@@ -1,8 +1,16 @@
 package errorhandling
 
 /**
+ *
+ *
+ *
  * @author erusak.
  */
 object EntryPoint extends App {
-  println("Hello world")
+
+  // Exercise 4_2
+  println(s"mean of (1,2,3,4,5,6,7,8) is ${mean(Seq[Double](1,2,3,4,5,6,7,8))}")
+  println(s"variance  of (1,2,3,4,5,6,7,8) is ${variance(Seq[Double](1,2,3,4,5,6,7,8))}")
+  println(s"mean of () is ${mean(Seq[Double]())}")
+  println(s"variance of () is ${variance(Seq[Double]())}")
 }
--- src/errorhandling/package.scala
+++ src/errorhandling/package.scala
@@ -24,4 +24,17 @@ package object errorhandling {
   }
   case class Some[+A](get: A) extends Option[A]
   case object None extends Option[Nothing]
+
+  // Exercise 4_2
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap {
+      //reusing mean hear
+      m => mean( xs.map( x => math.pow( x - m, 2)) )
+    }
+  }
+
+  def mean(xs: Seq[Double]): Option[Double] = {
+    if (xs.isEmpty) None else Some(xs.sum / xs.length)
+  }
+
 }
